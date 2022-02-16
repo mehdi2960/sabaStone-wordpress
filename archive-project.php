06581ba67fc57003596499f81bcfa85eab22afdf
mehdi <81480288+mehdi2960@users.noreply.github.com>
2022-02-16 18:54:31 +0330

    Update archive-project.php

--- archive-project.php
+++ archive-project.php
@@ -7,14 +7,14 @@
                     <section class="single-product-slogan big-padding marginTop" data-scroll-section>
                         <div class="row">
                             <div class="col-12 col-lg-6 col-xl-4">
-								   <div class="sP-info">
-										<h1 data-scroll data-scroll-repeat="true"><?php echo get_the_title();?></h1>
-										<div class="content" data-scroll data-scroll-repeat="true">
-											<p>
-											 <?php the_content();?>
-											</p>
-										</div>
-									</div>
+			   <div class="sP-info">
+					<h1 data-scroll data-scroll-repeat="true"><?php echo get_the_title();?></h1>
+					<div class="content" data-scroll data-scroll-repeat="true">
+						<p>
+						 <?php the_content();?>
+						</p>
+					</div>
+				</div>
                             </div>
 
                             <div class="col-12 col-lg-6 col-xl-8">
